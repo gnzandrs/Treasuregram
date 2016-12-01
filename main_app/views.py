0194b3495271cbe26d54f30399698e8b16f0fe66
gnzandrs <gavr@outlook.cl>
2016-11-30 21:42:57 -0300

    data from a object

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
-    name = 'Gold Nugget'
-    value = 1000.00
-    context = {'treasure_name': name,
-                'treasure_val': value}
+    return render(request, 'index.html', {'treasures': treasures})
 
-    return render(request, 'index.html', context)
+class Treasure:
+    def __init__(self, name, value, material, location):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+
+treasures = [
+    Treasure('Golden Nugget', 500.00, 'gold', "Curly's Creek, NM"),
+    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO"),
+    Treasure('Coffe Can', 20.00, 'tin', 'Acme CA')
+]
