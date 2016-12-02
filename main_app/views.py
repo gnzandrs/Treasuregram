9cdfaf2275398b78ba0307e8b6ca75c7adefd986
gnzandrs <gavr@outlook.cl>
2016-12-01 22:53:25 -0300

    template and image url to object

--- main_app/views.py
+++ main_app/views.py
@@ -4,14 +4,18 @@ def index(request):
     return render(request, 'index.html', {'treasures': treasures})
 
 class Treasure:
-    def __init__(self, name, value, material, location):
+    def __init__(self, name, value, material, location,img_url):
         self.name = name
         self.value = value
         self.material = material
         self.location = location
+        self.img_url = img_url
 
 treasures = [
-    Treasure('Golden Nugget', 500.00, 'gold', "Curly's Creek, NM"),
-    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO"),
-    Treasure('Coffe Can', 20.00, 'tin', 'Acme CA')
+    Treasure('Golden Nugget', 500.00, 'Gold', "Curly's Creek, NM",
+    'https://s3.amazonaws.com/courseware.codeschool.com/try_django/images/treasuregram-gold-nugget.png'),
+    Treasure("Fool's Gold", 0, 'Pyrite', "Fool's Falls, CO",
+    'https://s3.amazonaws.com/courseware.codeschool.com/try_django/images/treasuregram-fools-gold.png'),
+    Treasure('Coffee Can', 20.00, 'Tin', 'Acme CA',
+    'https://s3.amazonaws.com/courseware.codeschool.com/try_django/images/treasuregram-coffee-can.png')
 ]
