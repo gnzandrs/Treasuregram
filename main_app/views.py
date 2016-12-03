adfd5461bfad2e0400b92ecfee44bd06c7805bd7
gnzandrs <gavr@outlook.cl>
2016-12-03 14:23:16 -0300

    migration and model

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
+from .models import Treasure
 
 def index(request):
+    treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures': treasures})
-
-class Treasure:
-    def __init__(self, name, value, material, location,img_url):
-        self.name = name
-        self.value = value
-        self.material = material
-        self.location = location
-        self.img_url = img_url
-
-treasures = [
-    Treasure('Golden Nugget', 500.00, 'Gold', "Curly's Creek, NM",
-    'https://s3.amazonaws.com/courseware.codeschool.com/try_django/images/treasuregram-gold-nugget.png'),
-    Treasure("Fool's Gold", 0, 'Pyrite', "Fool's Falls, CO",
-    'https://s3.amazonaws.com/courseware.codeschool.com/try_django/images/treasuregram-fools-gold.png'),
-    Treasure('Coffee Can', 20.00, 'Tin', 'Acme CA',
-    'https://s3.amazonaws.com/courseware.codeschool.com/try_django/images/treasuregram-coffee-can.png')
-]
